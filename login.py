0a45f61c7d939e13b5e479a280d80dbc0e8ceb46
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2018-03-19 04:49:13 -0700

    login wip

--- login.py
+++ login.py
@@ -1,15 +1,29 @@
-import oauth2
-import constants
-import urllib.parse as urlparse
-
-consumer = oauth2.Consumer(constants.CONSUMER_KEY, constants.CONSUMER_SECRET)
+from user import User
+from db import Database
+from utils import get_request_token, get_oauth_verifier, get_access_token
 
 
-def oauth_authorized():
-    client = oauth2.Client(consumer)
+Database.initialize(user='postgres', password='', host='localhost', database='twitter_db')
 
-    response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
-    if response.status != 200:
-        print("An error occurred")
+user_email = input("Enter your e-mail address: ")
 
-    return dict(urlparse.parse_qsl(content.decode('utf-8')))
+user = User.load_from_db_by_email(user_email)
+
+if not user:
+    request_token = get_request_token()
+
+    oauth_verifier = get_oauth_verifier(request_token)
+
+    access_token = get_access_token(request_token, oauth_verifier)
+
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+
+    user = User(user_email, first_name, last_name, access_token['oauth_token'], access_token['oauth_token_secret'], None)
+    user.save_to_db()
+
+
+tweets = user.twitter_request('https://api.twitter.com/1.1/search/tweets.json?q=computers+filter:images')
+
+for tweet in tweets['statuses']:
+    print(tweet['text'])
