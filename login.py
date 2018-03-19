a2ef37d71fb78eb36253c73728306792290eb9c4
Matt Barraza-Carl <mccarl@asu.edu>
2018-03-19 05:02:39 -0700

    WIP login

--- login.py
+++ login.py
@@ -5,25 +5,25 @@ from utils import get_request_token, get_oauth_verifier, get_access_token
 
 Database.initialize(user='postgres', password='', host='localhost', database='twitter_db')
 
-user_email = input("Enter your e-mail address: ")
+# user_email = input("Enter your e-mail address: ")
 
-user = User.load_from_db_by_email(user_email)
+# user = User.load_from_db_by_email(user_email)
 
-if not user:
-    request_token = get_request_token()
+# if not user:
+#     request_token = get_request_token()
 
-    oauth_verifier = get_oauth_verifier(request_token)
+#     oauth_verifier = get_oauth_verifier(request_token)
 
-    access_token = get_access_token(request_token, oauth_verifier)
+#     access_token = get_access_token(request_token, oauth_verifier)
 
-    first_name = input("Enter your first name: ")
-    last_name = input("Enter your last name: ")
+#     first_name = input("Enter your first name: ")
+#     last_name = input("Enter your last name: ")
 
-    user = User(user_email, first_name, last_name, access_token['oauth_token'], access_token['oauth_token_secret'], None)
-    user.save_to_db()
+#     user = User(user_email, first_name, last_name, access_token['oauth_token'], access_token['oauth_token_secret'], None)
+#     user.save_to_db()
 
 
-tweets = user.twitter_request('https://api.twitter.com/1.1/search/tweets.json?q=computers+filter:images')
+tweets = user.twitter_request('https://api.twitter.com/1.1/search/tweets.json?q=')
 
 for tweet in tweets['statuses']:
     print(tweet['text'])
